766d1fdfd27b82c160da42d1cb7fcbc63932d233
Jason <jrosasrjf@gmail.com>
2021-06-16 23:26:07 -0500

    filled out more of the server with creating db and app.use

--- server.js
+++ server.js
@@ -7,3 +7,16 @@ const logger; // = require("");
 
 // app.use 
 app.use(logger("dev")); 
+app.use(express.urlencoded({extended: true})); 
+app.use(express.json());
+
+app.use(express.static("public"));
+
+// Creating the db 
+const databaseURL; // ="warmup for example"
+const collection; // = ["books for example"]
+
+const db = mangojs(databaseURL, collection); 
+db.on("error", error => {
+    console.log("Database Error: ", error); 
+}); 
