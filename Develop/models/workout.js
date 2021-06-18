a11b4c36856d2229a40705693be2410503f6147d
Jason <jrosasrjf@gmail.com>
2021-06-17 23:13:43 -0500

    began populating the homeroutes

--- Develop/models/workout.js
+++ Develop/models/workout.js
@@ -1,6 +1,6 @@
 // will be using mongoose schema templating
 
-const mongose = require("mongoose"); 
+const mongoose = require("mongoose"); 
 const Schema = mongoose.Schema; 
 
 // a collection of objects recording the workouts. As stated in the readme 
@@ -36,11 +36,21 @@ const workoutSchema = new Schema({
     },
 
     totalDuration: {
+        // well be referenced by the function below
         type: Number, 
         default: 0.0
     }
 
 });
 
+/* -- Running the totalTime -- */
+workoutSchema.methods.totalTime = () => {
+    for (let i = 0; i < this.exercise.length; i++){ // for this instance of the exercise object length 
+        this.totalDuration += this.exercise[i].duration; // default is 0.0 will keep increasing. 
+    }
+    return this.totalDuration; 
+}
+
+/* -- Exporting -- */
 const Workout = mongoose.model("Workout", workoutSchema);
-module.exports = Workout; // exports the module 
+module.exports = Workout; // referenced by the routes 
---
+++ Develop/routes/homeroutes.js
@@ -0,0 +1,7 @@
+/* -- variables -- */
+const express = require("express");
+const router = express.Router(); 
+const path = require("path");
+
+// calling the workout module 
+const Workout = require("../models/workout.js"); 
