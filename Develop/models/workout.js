7eb0eebf80cdf695e89d47d8c9144814c3ba864b
Jason <jrosasrjf@gmail.com>
2021-06-17 22:42:36 -0500

    placed the basic information of the exercise in the exercise schema

--- Develop/models/workout.js
+++ Develop/models/workout.js
@@ -1,21 +1,43 @@
-// will be using mongoose
+// will be using mongoose schema templating
 
 const mongose = require("mongoose"); 
 const Schema = mongoose.Schema; 
 
+// a collection of objects recording the workouts. As stated in the readme 
 const workoutSchema = new Schema({
-    // a collection of objects recording the workouts. As stated in the readme  
-
-    /*
-    I want to be able to log multiple exercises in a workout on a given day. 
-    I should also be able to track the name, type, weight, sets, reps, and duration of exercise. 
-    If the exercise is a cardio exercise, I should be able to track my distance traveled.
-    */
-
+    
     // the required template below. 
-    day: {}, 
-    totalDuration: {},    
-    exercise: {}
+    day: {
+        type: Date,         // defining the object type  
+        default: Date.now   // the standard value will result in the current date. 
+    }, 
+    
+    exercise: {
+        /* I should also be able to track the name, type, weight, sets, reps, and duration of exercise. 
+        If the exercise is a cardio exercise, I should be able to track my distance traveled. */
+        
+        name: {
+            type: String, 
+            trim: true, 
+            required: "What is the name of your exercise?"
+        }, 
+
+        type: {},
+
+        weight: {}, 
+        
+        sets: {}, 
+        
+        reps: {}, 
+        
+        duration: {}
+
+    },
+
+    totalDuration: {
+        type: Number, 
+        default: 0.0
+    }
 
 });
 
