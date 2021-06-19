dba02713c708942e92278314f1a15892428441a0
Jason <jrosasrjf@gmail.com>
2021-06-19 00:14:27 -0500

    updated api js file with requires

---
+++ Develop/routes/api.js
@@ -0,0 +1,25 @@
+/* -- Creating basic variable  -- */
+
+const express = require("express"); 
+const router = express.Router(); 
+const mongo = require("mongojs")
+const mongoose = require("mongoose"); 
+
+const Workout = require("../models/workout.js");
+
+
+/* -- the magic -- */
+
+
+
+
+
+
+
+
+
+
+
+
+/* -- exporting -- */
+module.exports = router; 
